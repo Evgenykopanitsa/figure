94e962132a302dfaed20085690772e41af610044
Student <Student@EMIS.local>
2018-02-24 18:40:23 +0700

    ffff

---
+++ ConsoleApplication4/ConsoleApplication4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp00
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(
+                " ____________\n" +
+                "|            |\n" +
+                "|            |\n" +
+                "|            |\n" +
+                "|            |\n" +
+                "|            |\n" +
+                "|____________|\n");
+        }
+
+    }
+}
